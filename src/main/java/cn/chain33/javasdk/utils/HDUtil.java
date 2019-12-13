62233959e57ee60dbfc81b9aff09ce873367c819
hbj <hbj@33.cn>
2019-12-13 18:26:40 +0800

    HD钱包Java代码生成例子

--- src/main/java/cn/chain33/javasdk/utils/HDUtil.java
+++ src/main/java/cn/chain33/javasdk/utils/HDUtil.java
@@ -64,21 +64,4 @@ public class HDUtil {
         byte[] publicKeyByte = HexUtil.fromHexString(publicKey);
         return TransactionUtil.genAddress(publicKeyByte);
     }
-
-    public static void main(String[] args) {
-//        String mnemonic = generateMnemonic("hbj123456");
-        String mnemonic = "weekend message replace tornado jump mail volume apology wheel peasant enemy dog";
-        System.out.println("mnemonic is : [" + mnemonic + "]");
-        int[] a = {1, 10};
-        List<HDWallet> hdWallets = createHDWalletByPath(mnemonic, "hbj123456", a);
-        for (HDWallet hdWallet : hdWallets) {
-            System.out.println("=========================");
-            System.out.println("address : " + hdWallet.getAddress());
-            System.out.println("pubKey  : " + hdWallet.getPubKey());
-            System.out.println("priKey  : " + hdWallet.getPriKey());
-            System.out.println("=========================");
-        }
-        String hexPublicKey = "0294a118763da4fb7157dad1309732c93bb125a5ad8da00e841abf14cfbffd837d";
-        System.out.println("公钥[" + hexPublicKey + "]生成的地址为[" + generateAddress(hexPublicKey) + "]");
-    }
 }
