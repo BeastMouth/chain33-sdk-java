b3f0f9d4bb8ff83d84f909680a1bd133c4dbdcaa
hbj <hbj@33.cn>
2019-12-13 19:55:44 +0800

    增加一些注释打印，后期删除

--- src/main/java/cn/chain33/javasdk/utils/HDUtil.java
+++ src/main/java/cn/chain33/javasdk/utils/HDUtil.java
@@ -23,6 +23,12 @@ public class HDUtil {
     private static Logger log = LoggerFactory.getLogger(HDUtil.class);
     static NetworkParameters PARAMS = new MainNetParams();
 
+    /**
+     * 生成助记词
+     *
+     * @param passphrase 密码
+     * @return 生成的助记词
+     */
     public static String generateMnemonic(String passphrase) {
         StringBuilder words = new StringBuilder();
         SecureRandom secureRandom = new SecureRandom();
@@ -34,18 +40,33 @@ public class HDUtil {
         return words.toString().trim();
     }
 
-    public static List<HDWallet> createHDWalletByPath(String word, String passphrase, int[] childNum) {
+    /**
+     * 生成分层钱包
+     *
+     * @param word       助记词
+     * @param passphrase 密码
+     * @param childNum   生成的分层钱包个数
+     * @return 生成分层钱包的结果
+     */
+    public static List<HDWallet> createHDWalletByPath(String walletPath, String word, String passphrase, int[] childNum) {
         List<HDWallet> wallet = new ArrayList<>();
         try {
+            // 保存种子节点到确定性钱包中
+            // seed：通过助记词和密码生成
             DeterministicSeed deterministicSeed = new DeterministicSeed(word, null, passphrase, 0L);
+            // 通过种子信息生成确定性钥匙链
             DeterministicKeyChain deterministicKeyChain = DeterministicKeyChain.builder().seed(deterministicSeed).build();
-            DeterministicKey main = deterministicKeyChain.getKeyByPath(HDUtils.parsePath("44H/0H"), true);
+            // 通过路径获得相对应的钥匙
+            DeterministicKey main = deterministicKeyChain.getKeyByPath(HDUtils.parsePath(walletPath), true);
             DeterministicHierarchy tree = new DeterministicHierarchy(main);
             DeterministicKey rootKey = tree.getRootKey();
+            System.out.println("root key : " + rootKey.toString());
             log.info("BTY childs privateKey,public,address start");
             for (int i = childNum[0], len = childNum[1]; i < len; i++) {
                 DeterministicKey deriveChildKey = HDKeyDerivation.deriveChildKey(rootKey, new ChildNumber(i));
+                System.out.println("path : " + deriveChildKey.getPath());
                 String path = deriveChildKey.getPathAsString();
+                System.out.println("path as string : " + deriveChildKey.getPathAsString());
                 String priKey = deriveChildKey.getPrivateKeyAsHex();
                 String pubKey = deriveChildKey.getPublicKeyAsHex();
                 String address = ECKey.fromPrivate(deriveChildKey.getPrivKey()).toAddress(PARAMS).toBase58();
@@ -60,6 +81,12 @@ public class HDUtil {
         return wallet;
     }
 
+    /**
+     * 通过公钥获取钱包地址
+     *
+     * @param publicKey 公钥
+     * @return 钱包地址
+     */
     public static String generateAddress(String publicKey) {
         byte[] publicKeyByte = HexUtil.fromHexString(publicKey);
         return TransactionUtil.genAddress(publicKeyByte);
--- src/test/java/cn/chain33/javasdk/client/HDWalletTest.java
+++ src/test/java/cn/chain33/javasdk/client/HDWalletTest.java
@@ -15,8 +15,8 @@ public class HDWalletTest {
     public void testHDWalletGenerate() {
         String mnemonic = HDUtil.generateMnemonic("hbj123456");
         System.out.println("mnemonic is : [" + mnemonic + "]");
-        int[] a = {1, 10};
-        List<HDWallet> hdWallets = HDUtil.createHDWalletByPath(mnemonic, "hbj123456", a);
+        int[] a = {0, 9};
+        List<HDWallet> hdWallets = HDUtil.createHDWalletByPath("/0/1H", mnemonic, "hbj123456", a);
         for (HDWallet hdWallet : hdWallets) {
             System.out.println("=========================");
             System.out.println("address : " + hdWallet.getAddress());
@@ -24,6 +24,10 @@ public class HDWalletTest {
             System.out.println("priKey  : " + hdWallet.getPriKey());
             System.out.println("=========================");
         }
+    }
+
+    @Test
+    public void generateAddress() {
         String hexPublicKey = "0294a118763da4fb7157dad1309732c93bb125a5ad8da00e841abf14cfbffd837d";
         System.out.println("公钥[" + hexPublicKey + "]生成的地址为[" + HDUtil.generateAddress(hexPublicKey) + "]");
     }
